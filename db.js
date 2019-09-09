0a3d352f1c2c4a72d65cc735669bb1f2fd158ad7
Viki Zhang <onlyzj@gmail.com>
2019-09-09 15:32:44 +0800

    add dotenv package to save the privacy of this project.

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mongodb = require("mongodb");
-const connectionString =
-  "mongodb+srv://ToDoApp-user1:123456a@cluster0-v8qpu.mongodb.net/complexApp?retryWrites=true&w=majority";
 
 mongodb.connect(
-  connectionString,
+  process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
     module.exports = client.db();
     const app = require("./app");
-    app.listen(3002);
+    app.listen(process.env.PORT);
   }
 );
